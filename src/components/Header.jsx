622bd848ddb374e31b8ed88a089abb6d2a5880aa
Askin <asknkskk@gmail.com>
2022-07-10 20:42:37 +0300

    buttons

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import Button from "./styles/Button.styled";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <Button color="#A62440">Apply Cources</Button>
+        <Button bg="#a62440">Talk to Adviser</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
